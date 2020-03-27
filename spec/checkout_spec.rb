bf5d0765c62523b8398d557ec87026087137a0bc
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-03-27 11:30:29 +0000

    #checkout takes ('B') and returns 30.

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,9 +2,14 @@ require 'checkout'
 
 describe Shop do
     describe '#checkout' do
+      subject(:shop) { described_class.new }
+      
       it "takes ('A') and returns 50" do
-        shop = Shop.new
         expect(shop.checkout('A')).to eq 50
+      end
+
+      it "takes ('B') and returns 30" do
+        expect(shop.checkout('B')).to eq 30
       end
     end
 end
